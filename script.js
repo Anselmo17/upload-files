0a7878dbf99525f034cb765de9213298574b29b3
Anselmo <ansinftec17@gmail.com>
2025-01-04 11:09:12 -0300

    FEAT:Adicionando metodos para salvar imagens e mostrar na galeria

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// variables global
+const imageInput = document.getElementById("image");
+const gallery = document.getElementById("gallery");
+const sectionGallery = document.querySelector(".section-gallery");
 const fileInput = document.getElementById("file");
 fileInput.addEventListener("change", handleFileSelect);
 
-const imageInput = document.getElementById("image");
+
 
 function handleFileSelect(event) {
   const file = event.target.files[0];
@@ -24,48 +28,47 @@ function readImageAsBlob(file) {
 
 function uploadFile() {
   const file = document.getElementById("file").files[0];
- this.saveImageAndBase64(file);
+  this.saveImageAndBase64(file);
 }
+
 
 
 function saveImageAndBase64(file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        const base64Image = reader.result; //.split(',')[1];
-        try {
-          const images = JSON.parse(localStorage.getItem('Images')) || [];
+  gallery.innerHTML = '';
+  const reader = new FileReader();
 
-          images.push({ image: base64Image });
-          const ImagesFormat = JSON.stringify(images);
+  reader.onload = () => {
+    const base64Image = reader.result;
+    try {
+      const images = JSON.parse(localStorage.getItem("Images")) || [];
 
-          localStorage.setItem('Images', ImagesFormat);
-          alert('Imagem salva com sucesso!');
-          this.showImageSave();
-        } catch (error) {
-          alert('Erro ao salvar a imagem: ' + error.message);
-        }
-      };
-      reader.readAsDataURL(file);
+      images.push({ image: base64Image });
+      const ImagesFormat = JSON.stringify(images);
+
+      localStorage.setItem("Images", ImagesFormat);
+      alert("Imagem salva com sucesso!");
+      this.showImageSave();
+    } catch (error) {
+      alert("Erro ao salvar a imagem: " + error.message);
+    }
+  };
+  reader.readAsDataURL(file);
 }
-
 
 function showImageSave() {
   // Verificar se a imagem está armazenada no localStorage
-  const imagesBase64 = JSON.parse(localStorage.getItem('Images'));
-  
+  const imagesBase64 = JSON.parse(localStorage.getItem("Images"));
+
   if (!imagesBase64) {
     console.log("Nenhuma imagem encontrada no localStorage.");
     return;
   }
-  
-  // Criar e exibir a imagem
-  let templateImages = '';
-  
-  imagesBase64.forEach(item => {
-    const img = document.createElement('img');
-    img.src = item.image;
-   templateImages += img;
-  });
 
-  document.getElementById('card').appendChild(templateImages);
+  // Criar e exibir a imagem
+  imagesBase64.forEach((item) => {
+    const img = document.createElement("img");
+    img.src = item.image;
+    gallery.appendChild(img);
+    sectionGallery.style.display = "block";
+  });
 }
